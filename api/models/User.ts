4df5ca6bd8d4a941d431f0d201e1d5116b7537c2
hanskyi <han.eginbaev@gmail.com>
2024-01-07 16:23:36 +0600

    add vercel.json

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,5 +1,5 @@
 import {Schema, model, Model, HydratedDocument} from "mongoose"
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import {randomUUID} from "crypto";
 import {IUser} from "../type";
 
