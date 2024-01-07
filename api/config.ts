cf3d01ac35a6c8571cd5689639f50f2eb78cb7f7
hanskyi <han.eginbaev@gmail.com>
2024-01-07 17:22:46 +0600

    add vercel.json

--- api/config.ts
+++ api/config.ts
@@ -5,7 +5,7 @@ const rootPath = __dirname;
 const config = {
     rootPath,
     publicPath: path.join(rootPath, 'public'),
-    db: 'mongodb+srv://haneginbaev:<08YpaSn6LBAJM4Ui>@cluster0.8krx5ff.mongodb.net/?retryWrites=true&w=majority',
+    db: 'mongodb+srv://haneginbaev:08YpaSn6LBAJM4Ui@cluster0.8krx5ff.mongodb.net/?retryWrites=true&w=majority/',
 };
 
 export default config;
