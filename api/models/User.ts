196da8f75b8169521ccaa415743a65847be103fa
hanskyi <han.eginbaev@gmail.com>
2024-01-07 22:05:20 +0600

    versel

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,5 +1,5 @@
 import {Schema, model, Model, HydratedDocument} from "mongoose"
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import {randomUUID} from "crypto";
 import {IUser} from "../type";
 
